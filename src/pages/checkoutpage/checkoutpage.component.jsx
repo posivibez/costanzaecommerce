ea1b498cd005a0924527964bff5209628e907577
posivibez <tgumb@sva.edu>
2021-08-13 12:46:10 -0700

    removed accidental yarn lock file

--- src/pages/checkoutpage/checkoutpage.component.jsx
+++ src/pages/checkoutpage/checkoutpage.component.jsx
@@ -4,9 +4,15 @@ import { connect } from "react-redux";
 
 import { createStructuredSelector } from "reselect";
 
-import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selectors";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../redux/cart/cart.selectors";
 
-import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
+
 import "./checkoutpage.styles.scss";
 
 const CheckoutPage = ({ cartItems, total }) => (
@@ -28,18 +34,26 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map((cartItem) => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)}
+    {cartItems.map((cartItem) => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
     <div className="total">
-        <span className="total">
-            TOTAL: ${total}
-        </span>
+      <span className="total">TOTAL: ${total}</span>
+      <br />
+      <StripeCheckoutButton price={total} />
+    </div>
+
+    <div className="test__warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 -- Exp: 01/20 -- CVV: 123
     </div>
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-    cartItems: selectCartItems,
-    total: selectCartTotal
-})
+  cartItems: selectCartItems,
+  total: selectCartTotal,
+});
 
 export default connect(mapStateToProps)(CheckoutPage);
